ef2b7993ce3c30651942c696b9a1ab18d94c62d1
“AjayVadapalli” <vajaysivasurya@gmail.com>
2024-09-23 21:23:20 +0530

    inheritance commit

---
+++ inheritance.java
@@ -0,0 +1,18 @@
+class vehicle{
+    public void start(){
+        System.out.println("Vehicle started");
+    }    
+}
+class car extends vehicle{
+    public void start(){
+        System.out.println("Car started");
+    }
+}
+public class inheritance{
+    public static void main(String[] args) {
+        vehicle v = new vehicle();
+        v.start();
+        car c =new car();
+        c.start();
+    }
+}
